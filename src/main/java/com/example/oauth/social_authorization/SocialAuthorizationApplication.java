15e83066f4b4633ccc80b2cea465b2a28ae258fa
Vijay Ramani <vijay.ramani5403@gmail.com>
2023-03-07 14:58:43 +0530

    Social Authorization in Spring boot

---
+++ src/main/java/com/example/oauth/social_authorization/SocialAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth.social_authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  SocialAuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialAuthorizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oauth/social_authorization/SocialAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth.social_authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialAuthorizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
